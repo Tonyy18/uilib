3b83f8b5b29717531bab2a674d2fc28e5f35433e
Toni Isotalo <46251776+Tonyy18@users.noreply.github.com>
2022-10-22 19:09:15 +0300

    Add files via upload

---
+++ uilib.js
@@ -0,0 +1,21 @@
+
+class Select {
+    
+}
+
+function initSelections() {
+    const selects = $("select");
+    selects.each(function() {
+        
+    })
+}
+window.onload = function() {
+    if(typeof $ != "function") {
+        //Check if jquery is found
+        console.error("Jquery is not found")
+        return;
+    }
+
+
+
+}
